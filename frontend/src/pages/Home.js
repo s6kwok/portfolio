d208caeecafcab403d028604a7bba54cb6a7ae69
Stacy Kwok <stacykwok@staces-MacBook-Pro.local>
2024-10-02 11:51:35 -0400

    cleaned up old code comments

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,7 +15,6 @@ export default function Home() {
             style={{
                 height: '68vh',
                 display: 'flex',
-                // justifyContent: 'center',
                 alignItems: 'center',
                 marginLeft: '6.25rem',
                 marginRight: '6.25rem',
@@ -24,60 +23,32 @@ export default function Home() {
                 marginBottom: '6.5rem',
             }}
             >
-                <div style={{ 
-                    // textAlign: 'center', 
-                    }}>
+                <div>
+                    <h1>Hi, I’m Stacy!</h1>
                     <div
-                        style={{
-                            // marginLeft: '12rem',
-                            // marginRight: '14rem',
-                        }}
+                    style={{
+                        textAlign: 'left',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-start',
+                        justifyContent: 'flex-start',
+                        paddingTop: '0.75rem',
+                    }}
                     >
-                        <h1>Hi, I’m Stacy!</h1>
-                        {/* <h1>I bring people and data together to design impactful and elegant solutions.</h1> */}
-                        {/* <div
-                            style={{
-                                textAlign: 'center',
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                flexWrap: 'wrap',
-                                paddingTop: '0.75rem',
-                            }}
-                        >
-                            <h1 style={{paddingRight: '1rem',}}>I bring </h1>
-                            <Tag theme="primaryBlue" size="medium" icon={RiEmotionHappyLine} text="people" />
-                            <h1 style={{paddingRight: '1rem', paddingLeft: '1rem',}}> and </h1>
-                            <Tag theme="orange" size="medium" icon={BsBarChartLineFill} text="data" />
-                            <h1 style={{lineHeight: '5.75rem', paddingTop: '0.75rem',}}> together to design impactful and elegant solutions.</h1>
-                        </div> */}
-                        <div
-                        style={{
-                            textAlign: 'left',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'flex-start',
-                            justifyContent: 'flex-start',
-                            paddingTop: '0.75rem',
-                            // marginLeft: '12rem',
-                        }}
-                        >
-                            {/* First line */}
-                            <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'nowrap' }}>
-                                <h1 style={{ paddingRight: '1rem' }}>I bring </h1>
-                                <Tag theme="primaryBlue" size="large" icon={RiEmotionHappyLine} text="people" />
-                                <h1 style={{ paddingRight: '1rem', paddingLeft: '1rem' }}> and </h1>
-                                <Tag theme="orange" size="large" icon={BsBarChartLineFill} text="data" />
-                                <h1 style={{ paddingLeft: '1rem' }}> together</h1>
-                            </div>
-
-                            {/* Second line */}
-                            <div style={{ paddingTop: '0.65rem', paddingBottom: '2.25rem' }}>
-                                <h1 style={{ margin: 0 }}>to design impactful and elegant solutions.</h1>
-                            </div>
+                        {/* First line */}
+                        <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'nowrap' }}>
+                            <h1 style={{ paddingRight: '1rem' }}>I bring </h1>
+                            <Tag theme="primaryBlue" size="large" icon={RiEmotionHappyLine} text="people" />
+                            <h1 style={{ paddingRight: '1rem', paddingLeft: '1rem' }}> and </h1>
+                            <Tag theme="orange" size="large" icon={BsBarChartLineFill} text="data" />
+                            <h1 style={{ paddingLeft: '1rem' }}> together</h1>
                         </div>
-                        <p>Prev @Questrade, @Jam City, @LCBO</p>
+                        {/* Second line */}
+                        <div style={{ paddingTop: '0.65rem', paddingBottom: '2.25rem' }}>
+                            <h1 style={{ margin: 0 }}>to design impactful and elegant solutions.</h1>
+                        </div>
                     </div>
+                    <p>Prev @Questrade, @Jam City, @LCBO</p>
                 </div>
             </div>
             <div
